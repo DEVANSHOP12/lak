2632bfc990580bcd8c51db13109a79cb194c9cc4
LAKSHYAWALA <101128540+DEVANSHOP12@users.noreply.github.com>
2023-11-07 23:47:56 +0530

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,3 +1,5 @@
+ document.addEventListener("DOMContentLoaded", function () {
+  // Your JavaScript code here
 
     // Function to replace the links in the table
     function replaceLinks() {
@@ -9,9 +11,9 @@
         for (let j = 0; j < linkButtons.length; j++) {
           const button = linkButtons[j];
           const currentLink = button.getAttribute('onclick');
-          if (currentLink.includes('https://lakpaid.blogspot.com/p/player-page.html')) {
+          if (currentLink.includes('https://xyz1234xd.blogspot.com/p/player.html')) {
             const newLink = currentLink.replace(
-              'https://lakpaid.blogspot.com/p/player-page.html',
+              'https://xyz1234xd.blogspot.com/p/player.html',
               'https://devanshop12.github.io/lak/player.html'
             );
             button.setAttribute('onclick', newLink);
@@ -21,9 +23,12 @@
     }
   
     // Call the function to replace links when the page loads
-    window.onload = replaceLinks;
-
+    window.onload = replaceLinks();
 console.log("bhai mai jinda hu ")
+
+     });
+
+
 
   
   
