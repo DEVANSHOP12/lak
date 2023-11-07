e30ebb59ead49557427dfa6d88824986cc7e8e74
LAKSHYAWALA <101128540+DEVANSHOP12@users.noreply.github.com>
2023-11-07 19:41:46 +0530

    Create main1.js

---
+++ main1.js
@@ -0,0 +1,27 @@
+
+
+    // Function to replace the links in the table
+    function replaceLinks() {
+      const table = document.getElementById('myTable');
+      const rows = table.getElementsByTagName('tr');
+      for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const linkButtons = row.getElementsByClassName('btn');
+        for (let j = 0; j < linkButtons.length; j++) {
+          const button = linkButtons[j];
+          const currentLink = button.getAttribute('onclick');
+          if (currentLink.includes('https://lakpaid.blogspot.com/p/player-page.html')) {
+            const newLink = currentLink.replace(
+              'https://lakpaid.blogspot.com/p/player-page.html',
+              'https://devanshop12.github.io/lak/player.html'
+            );
+            button.setAttribute('onclick', newLink);
+          }
+        }
+      }
+    }
+  
+    // Call the function to replace links when the page loads
+    window.onload = replaceLinks;
+  
+  
